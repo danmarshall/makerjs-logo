b8b9ec7e0464a4418daa5c089c800066fa953a16
Dan Marshall <danmar@microsoft.com>
2016-04-10 12:49:53 -0700

    rewritten for makerjs 0.7.2

--- index.js
+++ index.js
@@ -7,134 +7,125 @@ var Line = paths.Line;
 var Parallel = paths.Parallel;
 var model = makerjs.model;
 
-function logo(or, ir, ear, outline, mHeight, serifHeight, speed, drop, columnWidth, spacing, step) {
+function logo(outerRadius, innerRadius, serifLength, outline, mHeight, serifHeight, vAngle, hAngle, columnWidth, spacing, step, joints) {
 
     if (arguments.length == 0) {
         var v = makerjs.kit.getParameterValues(logo);
-        or = v.shift();
-        ir = v.shift();
-        ear = v.shift();
+        outerRadius = v.shift();
+        innerRadius = v.shift();
+        serifLength = v.shift();
         outline = v.shift();
         mHeight = v.shift();
         serifHeight = v.shift();
-        speed = v.shift();
-        drop = v.shift();
+        vAngle = v.shift();
+        hAngle = v.shift();
         columnWidth = v.shift();
         spacing = v.shift();
         step = v.shift();
     }
-
-    function M() {
-        this.models = {
-            base: {
-                paths: {}
-            },
-            legs: {
-                models: {}
-            }
-        };
-    }
-
-    var m_letter = new M();
-    var m_outline = new M();
     
-    var legModels = m_letter.models.legs.models;
-    var outlineModels = m_outline.models.legs.models;
+	function vGuides(centerX, rightOffset, leftOffset, angle) {
+		this.paths = {
+            left: new Line([centerX - leftOffset, 0], [centerX - leftOffset, 1]),
+			center: new Line([centerX, 0], [centerX, 1]),
+			right: new Line([centerX + rightOffset, 0], [centerX + rightOffset, 1])
+		}
+		makerjs.model.rotate(this, angle, [0, 0]);
+	}
 
-    var far = 100;
-
-    function addLeg(id, leftRef, leftSpace, topRef, topSpace, topPoint, trimToLeftRef, earDistance) {
-        var leg = {
-            paths: {}
-        };
-
-        leg.paths.top = new Parallel(topRef, topSpace, topPoint);
-        leg.paths.left = new Parallel(leftRef, leftSpace, [far, 0]);
-        leg.paths.serif = new Parallel(leg.paths.top, serifHeight, [0, 0]);
-        leg.paths.right = new Parallel(leg.paths.left, columnWidth, [far, 0]);
-        leg.paths.ear = new Parallel(leg.paths.left, earDistance, [0, 0]);
-
-        legModels[id] = leg;
-
-        var outleg = {
-            paths: {}
-        };
-
-        outleg.paths.top = new Parallel(leg.paths.top, outline, [0, far]);
-        outleg.paths.left = new Parallel(leg.paths.left, outline, [-far, 0]);
-        outleg.paths.serif = new Parallel(leg.paths.serif, outline, [0, 0]);
-        outleg.paths.right = new Parallel(leg.paths.right, outline, [far, 0]);
-        outleg.paths.ear = new Parallel(leg.paths.ear, outline, [-far, 0]);
-
-        outlineModels[id] = outleg;
+	function hGuides(lowerOffset, height, angle) {
+      	height = Math.max(height, lowerOffset);
+		this.paths = {  
+			top: new Line([0, height], [1, height]),
+ 			lower: new Line([0, height - lowerOffset], [1, height - lowerOffset])
+ 		}
+		makerjs.model.rotate(this, hAngle, [0, 0]);
+        this.paths.base = new Line([0, 0], [1, 0]);
+ 	}
+  
+  	function connectLines(line1, prop1, line2, prop2) {
+      	line1[prop1] = line2[prop2] = makerjs.point.fromSlopeIntersection(line1, line2);
     }
 
-    function trimLeg(id) {
-
-        function trimLegPart(leg, innerRadius, outerRadius) {
-            trimLines(leg.paths.top, leg.paths.right);
-            trimLines(leg.paths.serif, leg.paths.left);
-            leg.paths.innerFillet = path.fillet(leg.paths.left, leg.paths.serif, innerRadius);
-            leg.paths.outerFillet = path.fillet(leg.paths.top, leg.paths.right, outerRadius);
-            trimLines(leg.paths.top, leg.paths.ear, true);
-            trimLines(leg.paths.serif, leg.paths.ear, true, true);
+ 	function column(centerX, height, leftX) {
+        
+ 		var v = new vGuides(centerX, columnWidth, leftX, -vAngle);
+ 		var h = new hGuides(serifHeight, height - serifHeight);
+    
+      	if (leftX) {
+      		connectLines(v.paths.left, 'origin', h.paths.lower, 'origin');
+      		connectLines(v.paths.left, 'end', h.paths.top, 'origin');
+	      	connectLines(v.paths.center, 'end', h.paths.lower, 'end');
+        } else {
+          connectLines(v.paths.center, 'end', h.paths.top, 'origin');
+          delete h.paths.lower;
         }
-
-        trimLegPart(legModels[id], ir, or);
-        trimLegPart(outlineModels[id], ir - outline, or + outline);
-    }
-
-    function combineM(m) {
-        var legs = m.models.legs;
-        model.combine(legs.models['1'], legs.models['2'], false, true, false, true);
-        model.combine(legs.models['2'], legs.models['3'], false, true, false, true);
-        model.combine(m.models.base, legs, true, false, false, true);
-    }
-
-    m_outline.models.base.paths.bottom = new Line([0, 0], [far, 0]);
-    m_letter.models.base.paths.bottom = new Parallel(m_outline.models.base.paths.bottom, outline, [0, far]);
-
-    var rotatedLeft = path.rotate(new Line([0, 0], [0, far]), -speed, [0, 0]);
-    var rotatedBottom = path.rotate(new Line([-far, outline], [far, outline]), drop, [0, outline]);
-
-    addLeg('1', rotatedLeft, outline, rotatedBottom, mHeight, [0, far], false, ear + ir);
-    addLeg('2', legModels['1'].paths.right, spacing, legModels['1'].paths.top, step, [0, 0], true, spacing + columnWidth / 2);
-    addLeg('3', legModels['2'].paths.right, spacing, legModels['2'].paths.top, step, [0, 0], true, spacing + columnWidth / 2);
-
-    trimLeg('1', false);
-    trimLeg('2', true);
-    trimLeg('3', true);
-
-    combineM(m_letter);
-    combineM(m_outline);
-
+      	connectLines(v.paths.right, 'end', h.paths.top, 'end');
+      	connectLines(v.paths.center, 'origin', h.paths.base, 'origin');
+      	connectLines(v.paths.right, 'origin', h.paths.base, 'end');
+        
+      	this.paths = { 
+          outerFillet: makerjs.path.fillet(v.paths.right, h.paths.top, outerRadius)
+        };
+      	
+      	if (leftX) {
+          this.paths.innerFillet = makerjs.path.fillet(v.paths.center, h.paths.lower, innerRadius);
+        }
+      
+      	makerjs.extendObject(this.paths, h.paths);
+      	makerjs.extendObject(this.paths, v.paths);
+ 	}
+  	
     this.models = {
-        letter: m_letter,
-        outline: m_outline
+		column1:  new column(0, mHeight, serifLength)
     };
 
-}
+	var column2 = new column(spacing + columnWidth, mHeight - step, columnWidth + spacing - .00001);
+  	makerjs.model.combine(this, column2);
+    this.models.column2 = column2;
+      
+	var column3 = new column((spacing + columnWidth) * 2, mHeight - step * 2, columnWidth + spacing - .00001);
+  	makerjs.model.combine(this, column3);
+    this.models.column3 = column3;
 
-function trimLines(line1, line2, useLine1Origin, useLine2Origin) {
-    var int = path.slopeIntersectionPoint(line1, line2);
-    if (int) {
-        line1[useLine1Origin ? 'origin' : 'end'] = int;
-        line2[useLine2Origin ? 'origin' : 'end'] = int;
-    }
+    //make a clone of the M and remove the spaces between its columns
+  	var clone = makerjs.cloneObject(this);
+  	var m = clone.models;
+  
+  	delete m.column1.paths.right;
+
+  	delete m.column2.paths.base;
+  	delete m.column2.paths.right;
+
+  	delete m.column2.paths.center;
+  	delete m.column2.paths.lower;
+  	delete m.column2.paths.innerFillet;
+  	
+  	delete m.column3.paths.center;
+  	delete m.column3.paths.lower;
+  	delete m.column3.paths.innerFillet;
+  
+  	m.column3.paths.base.origin = m.column1.paths.base.end;
+  
+  	this.models.outline = makerjs.model.outline(clone, outline, joints);
+  	makerjs.model.simplify(this.models.outline);
 }
 
 logo.metaParameters = [
-    { title: "outer radius", type: "range", min: 0, max: 1.7, step: .1, value: 1.06 },
-    { title: "inner radius", type: "range", min: 0, max: .9, step: .1, value: .3 },
-    { title: "ear", type: "range", min: 0, max: 2, step: .1, value: 1.1 },
-    { title: "outline", type: "range", min: 0.2, max: 2, step: .1, value: 1.06 },
-    { title: "m height", type: "range", min: 7, max: 20, step: .1, value: 8.3 },
+    { title: "outer radius", type: "range", min: 0, max: 1.7, step: .1, value: 1.1 },
+    { title: "inner radius", type: "range", min: 0, max: .9, step: .1, value: .4 },
+    { title: "serif length", type: "range", min: 0, max: 2, step: .1, value: 1.3 },
+    { title: "outline", type: "range", min: 0.2, max: 2, step: .1, value: .9 },
+    { title: "m height", type: "range", min: 7, max: 20, step: .1, value: 8.2 },
     { title: "serif height", type: "range", min: .1, max: 1.9, step: .1, value: .65 },
-    { title: "speed", type: "range", min: 0, max: 45, step: 1, value: 19.01 },
-    { title: "drop", type: "range", min: 0, max: 30, step: 1, value: 1 },
-    { title: "column width", type: "range", min: .4, max: 5, step: .1, value: 2.59 },
-    { title: "spacing", type: "range", min: 1.3, max: 5, step: .1, value: 1.25 },
-    { title: "step", type: "range", min: 0, max: 4, step: .1, value: 2.385 },
+    { title: "vertical angle", type: "range", min: -45, max: 45, step: 1, value: 19 },
+    { title: "horizontal angle", type: "range", min: -15, max: 45, step: 1, value: 2 },
+    { title: "column width", type: "range", min: .4, max: 5, step: .1, value: 2.2 },
+    { title: "spacing", type: "range", min: .1, max: 5, step: .1, value: 1.3 },
+    { title: "step", type: "range", min: 0, max: 4, step: .1, value: 2.2 },
+    { title: "outline joints", type: "range", min: 0, max: 2, step: 1, value: 1 },
 ];
+
+logo.notes = 'This logo model has been re-written to use the new **model.outline** feature in Maker.js version 0.7.2';
 
 module.exports = logo;
